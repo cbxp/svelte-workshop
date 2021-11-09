cafc9755a57df867c5f3e16a186839fc40bef3ff
Anton Keks <anton@codeborne.com>
2021-11-09 17:43:11 +0200

    add sinon and sinon-chai for stubbing and verifications in tests

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,5 +1,15 @@
 process.env.NODE_ENV = 'test';
 
 module.exports = {
+  testsFinishTimeout: 10000,
+  // manual: true,
+  // open: true,
   plugins: [require('@snowpack/web-test-runner-plugin')()],
-};
+  testRunnerHtml: testFramework =>
+    `<html>
+      <body>
+        <script type="module" src="/dist/setup-tests.js"></script>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`
+}
